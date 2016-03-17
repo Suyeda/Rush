aba3090bc724effa182ab310587b5e3f75f92e1e
Sean Suyeda <seansuyedaSuyeda@RUXIN.local>
2016-03-16 22:53:32 -0700

    Changed buttons to customized a href tags in rails

--- app/assets/javascripts/functions.js
+++ app/assets/javascripts/functions.js
@@ -15,15 +15,15 @@ $(window).scroll(function(){
 
   if(wScroll > $('#portfolio').offset().top - $(window).height()){
 
-     var offset = (Math.min(0, wScroll - $('#portfolio').offset().top +$(window).height() - 850)).toFixed();
+     var offset = (Math.min(0, wScroll - $('#portfolio').offset().top +$(window).height() - 750)).toFixed();
 
      $('.post1').css({'transform': 'translate('+ offset + "." + 1 + 'px, '+ Math.abs(offset * 0.1) +'px)'});
 
-     var offset2 = (Math.min(0, wScroll - $('#portfolio').offset().top +$(window).height() - 950)).toFixed();
+     var offset2 = (Math.min(0, wScroll - $('#portfolio').offset().top +$(window).height() - 850)).toFixed();
 
      $('.post2').css({'transform': 'translate('+ offset2 + "." + 2 + 'px, '+ Math.abs(offset2 * 0.1) +'px)'});
 
-      var offset3 = (Math.min(0, wScroll - $('#portfolio').offset().top +$(window).height() - 1050)).toFixed();
+      var offset3 = (Math.min(0, wScroll - $('#portfolio').offset().top +$(window).height() - 950)).toFixed();
 
      $('.post3').css({'transform': 'translate('+ offset3 + "." + 3 + 'px, '+ Math.abs(offset * 0.1) +'px)'});
 
