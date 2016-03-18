6f38197496c82ff5660905667a09a06828881f1f
Sean Suyeda <seansuyedaSuyeda@RUXIN.local>
2016-03-17 22:59:49 -0700

    Adjusted jquery scroll speed on projects, and added font awesome to contact

--- app/assets/javascripts/functions.js
+++ app/assets/javascripts/functions.js
@@ -19,11 +19,11 @@ $(window).scroll(function(){
 
      $('.post1').css({'transform': 'translate('+ offset + "." + 1 + 'px, '+ Math.abs(offset * 0.1) +'px)'});
 
-     var offset2 = (Math.min(0, wScroll - $('#portfolio').offset().top +$(window).height() - 850)).toFixed();
+     var offset2 = (Math.min(0, wScroll - $('#portfolio').offset().top +$(window).height() - 1050)).toFixed();
 
      $('.post2').css({'transform': 'translate('+ offset2 + "." + 2 + 'px, '+ Math.abs(offset2 * 0.1) +'px)'});
 
-      var offset3 = (Math.min(0, wScroll - $('#portfolio').offset().top +$(window).height() - 950)).toFixed();
+      var offset3 = (Math.min(0, wScroll - $('#portfolio').offset().top +$(window).height() - 1350)).toFixed();
 
      $('.post3').css({'transform': 'translate('+ offset3 + "." + 3 + 'px, '+ Math.abs(offset * 0.1) +'px)'});
 
